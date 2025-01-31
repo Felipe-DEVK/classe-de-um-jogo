a2567198f667d277e1232293f774b2f45b25f2eb
Felipe Dev <felipedeveloper1k@gmail.com>
2025-01-31 14:21:09 +0100

    Todos as classes e mensagens feitas

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
  class Hero {
-    constructor(nome, idade, classe){
+    constructor(nome, idade, classe, ataque){
         this.nome = nome
         this.idade = idade
         this.classe = classe
+        this.ataque = ataque
     }
 
     toString(){
-        console.log
+        console.log(`o ${this.classe} atacou usando ${this.ataque}`)
     }
  }
+
+ let mago = new Hero ("Felipe" , "29", "Mago", "Magia")
+
+ let guerreiro = new Hero("Marcus", "25", "Guerreiro", "Espada")
+
+ let monge = new Hero("Leonardo", "30", "Monge", "Artes Marciais")
+
+ let ninja = new Hero("Carol", "30", "Ninja", "Shuriken")
+
+ mago.toString()
+ guerreiro.toString()
+ monge.toString()
+ ninja.toString()
