aa6e16a344413c38eee800d9afb840edd34ff1bb
Felipe Dev <felipedeveloper1k@gmail.com>
2025-01-31 13:58:36 +0100

    Meu Primeiro Commit Para o Desafio

---
+++ index.js
@@ -0,0 +1,11 @@
+ class Hero {
+    constructor(nome, idade, classe){
+        this.nome = nome
+        this.idade = idade
+        this.classe = classe
+    }
+
+    toString(){
+        console.log
+    }
+ }
